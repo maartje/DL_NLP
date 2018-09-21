2e555578022810773818f6e7e921e942d8e54029
maartje <maartjedejonge@gmail.com>
2018-09-21 14:24:16 +0200

    refactor mock file system

---
+++ tests/mock_file_system.py
@@ -0,0 +1,51 @@
+mocked_file_storage = {
+    'data/wili-2018/x_train.txt' : [
+        'Dit is een konijn',
+        'It is so nice to meet you',
+        'Het is een mooie dag',
+        'Hoe gaat het met je?',
+        'I feel very happy',
+        'Did you see what happened?'
+    ],
+    'data/wili-2018/y_train.txt' : [
+        'nld',
+        'eng',
+        'nld',
+        'nld',
+        'eng',
+        'eng'
+    ],
+    'data/wili-2018/x_test.txt' : [
+        'Er is een kat in de boom',
+        'Het is slecht weer vandaag',
+        'Why are you crying?',
+        'What is wrong whith you?'
+    ],
+    'data/wili-2018/y_test.txt' : [
+        'nld',
+        'nld',
+        'eng',
+        'eng'
+    ]
+}
+
+def mock_torch_load(fpath):
+    return mocked_file_storage[fpath]
+
+def mock_torch_save(data, fpath):
+    mocked_file_storage[fpath] = data
+
+def mock_save_file(data, fpath):
+    mocked_file_storage[fpath] = data
+
+def mock_read_file(fpath):
+    return mocked_file_storage[fpath]
+
+# fn_steps are 'main()' functions
+# make sure to use the mock functions
+def perform_steps(fn_steps = []): 
+    for fn_step in fn_steps:
+        fn_step()
+
+
+
